bbcbe5cdab8734b8929d520a8ac456570ad14bf3
tangxq <tangxiaoqiang@chinawbb.com>
2018-12-17 19:47:01 +0800

    success

---
+++ precache-manifest.36038d24d0f3a4571630ce4b8795fda5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e44c4c83e4617a8f0c17",
+    "url": "/static/js/main.e44c4c83.chunk.js"
+  },
+  {
+    "revision": "a698623097657255aed1",
+    "url": "/static/js/1.a6986230.chunk.js"
+  },
+  {
+    "revision": "e44c4c83e4617a8f0c17",
+    "url": "/static/css/main.3b9b8667.chunk.css"
+  },
+  {
+    "revision": "a698623097657255aed1",
+    "url": "/static/css/1.c2dc828a.chunk.css"
+  },
+  {
+    "revision": "e5fe6031deea61113b89a0477928fbf4",
+    "url": "/index.html"
+  }
+];
